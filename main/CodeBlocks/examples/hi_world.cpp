c5cadd12c13a950ba27c48fe12213c42254f1f42
Arturo Sanchez <arturo66cta@gmail.com>
2022-09-15 17:51:48 -0300

    change principal directory name from cpp to main

---
+++ main/CodeBlocks/examples/hi_world.cpp
@@ -0,0 +1,9 @@
+// Programa para imprimir uma linha de texto simples
+// hi_world.cpp
+#include <iostream> // Permite imprimir dados na tela
+
+int main()// A função main indica o início do programa
+{
+	std::cout << "Hello World!!" << std::endl;
+	return 0;
+} // final da função main
---
+++ main/Introducao/examples/hi_world_name_v3.cpp
@@ -0,0 +1,9 @@
+// Programa para imprimir uma linha de texto simples
+// hi_world_name3.cpp
+#include <iostream> // Permite imprimir dados na tela
+
+int main()// A função main indica o início do programa
+{
+	std::cout << "Hello World,\nmy name is Gisella.\nHi everybody";
+	return 0;
+} // final da função main
