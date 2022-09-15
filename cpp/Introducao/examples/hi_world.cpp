1fa99275d7441dfd57140fe9a4379dde19604c72
Arturo Sanchez <arturo66cta@gmail.com>
2022-09-15 17:53:32 -0300

    delete files into cpp

--- cpp/Introducao/examples/hi_world.cpp
+++
@@ -1,9 +0,0 @@
-// Programa para imprimir uma linha de texto simples
-// hi_world.cpp
-#include <iostream> // Permite imprimir dados na tela
-
-int main()// A função main indica o início do programa
-{
-	std::cout << "Hello World!!" << std::endl;
-	return 0;
-} // final da função main
